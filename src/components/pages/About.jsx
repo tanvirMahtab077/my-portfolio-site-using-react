6ca1d375195cd9f998f190d3e706dbe25b00cf90
tanvir <shovontanvir124@gmail.com>
2022-04-11 01:46:49 +0600

    added about page content

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { workExperiences } from "../../json/workingExperiences";
 
 class About extends Component {
-    render() {
-        return (
-            <div>
-                <h2>about page</h2>
+  render() {
+    return (
+      <div className="container mt-5">
+        <div className="card text-center">
+          <h5 className="card-header main-header">About Me</h5>
+          <div className="card-body">
+            <p className="card-text">
+              <b>
+                Experienced Full Stack Web Developer with a demonstrated history
+                of working in the internet industry. Skilled in RESTful
+                webservices, Javascript,Nodejs,MERN stack
+                development,MySQL,MongoDB,PHP Development.Strong engineering
+                professional graduated from B.SC in CSE at East West University
+                Bangladesh.
+              </b>
+            </p>
+          </div>
+        </div>
+        <div className="card text-center mt-5">
+          <h5 className="card-header main-header">Working experience</h5>
+          <div className="container mt-4 mb-4">
+            <div className="row">
+              {workExperiences.map((data) => {
+                return (
+                  <div className="col">
+                    <div className="card mx-auto" style={{ maxWidth: "18rem" }}>
+                      <div id="cardBody" className="card-body">
+                        <h5>{data.company}</h5>
+                        <p>{data.designation}<br/>{data.joiningDate}</p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default About;
---
+++ src/json/workingExperiences.js
@@ -0,0 +1,8 @@
+export const workExperiences = [
+    {
+      company: "Omex Courier & Logistics Ltd.",
+      designation:"Software Engineer",
+      joiningDate:"Sep 2021-present"
+      
+    }
+  ];

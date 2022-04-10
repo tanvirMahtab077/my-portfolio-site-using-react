a25c3de61f6162c0e2ba61c9b0ee54758c7ee7ff
tanvir <shovontanvir124@gmail.com>
2022-04-10 23:58:44 +0600

    maked card-title responsive

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,28 +5,27 @@ class Home extends Component {
   render() {
     return (
       <div className="container mt-5">
-        <div
-          className="card text-center"          
-        >
+        <div className="card text-center">
           <img
             className="card-img-top mx-auto mt-5"
             src={headerImage}
             alt="Card image cap"
           />
-          <div className="card-body m-5">
-            <h5 className="card-title">Software Engineer</h5>
+          <div className="mt-4">
+            <h1>Tanvir Mahtab</h1>
+          </div>
+          <div className="card-body">
+            <h5 className="card-title mx-auto">Software Engineer</h5>
             <p className="card-text">
-              An organised and motivated employee capable of time management and
-              working under pressure in all environments. Seeking to join Acme
-              Corp as an Administrative Assistant to assist in ensuring good
-              internal communications and budget management.
+              Self-motivated, highly passionate, hardworking person and one year experiences looking
+              for an opportunity to work in a challenging organization to utilize my skills and knowledge to work for the growth of the organization.
             </p>
             <a
               href="/"
-              className="btn mx-auto"
-              style={{ backgroundColor: "rgb(74, 199, 141)", color: "white"}}
+              className="btn btn-xs mx-auto"
+              style={{ backgroundColor: "rgb(74, 199, 141)", color: "white" }}
             >
-             Download Resume
+              Download Resume
             </a>
           </div>
         </div>
